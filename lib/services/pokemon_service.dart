5b47d228292d7930cbe1cbeedbdd5d17a1224a36
Nathália Pissuti <nathaliapissuti@gmail.com>
2023-05-27 11:47:28 -0300

    Pokemon service

--- lib/services/pokemon_service.dart
+++ lib/services/pokemon_service.dart
@@ -1,5 +1,51 @@
+import 'dart:developer';
+
 import 'package:http/http.dart' as http;
 
+import 'package:pokedex/models/pokemon_model.dart';
 
-// Reference: https://github.com/PokeAPI/pokeapi/issues/661
+class ApiConstants {
+  static String baseUrl = 'https://pokeapi.co/api/v2';
+  static String pokemonEndpoint = '/pokemon';
+}
 
+class PokemonService {
+  Future<PokemonModel?> getPokemon(int id) async {
+    try {
+      var url = Uri.parse(
+          '${ApiConstants.baseUrl}${ApiConstants.pokemonEndpoint}/$id');
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        PokemonModel pokemon = pokemonFromJson(response.body);
+
+        var urlSpecies = Uri.parse(pokemon.species.url);
+        var responseSpecies = await http.get(urlSpecies);
+
+        if (responseSpecies.statusCode == 200) {
+          SpeciesModel species = speciesFromJson(responseSpecies.body);
+
+          var urlEvolutionChain = Uri.parse(species.evolutionChain.url);
+          var responseEvolutionChain = await http.get(urlEvolutionChain);
+
+          if (responseEvolutionChain.statusCode == 200) {
+            EvolutionChainModel evolutionChain =
+                evolutionChainFromJson(responseEvolutionChain.body);
+
+            pokemon.evolutionChain = evolutionChain;
+          }
+        }
+
+        return pokemon;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+
+    return null;
+  }
+
+  Future<List<PokemonModel?>> getPokemonList() async {
+    var numbers = List<int>.generate(150, (i) => i + 1);
+    return await Future.wait(numbers.map((number) => getPokemon(number)));
+  }
+}
