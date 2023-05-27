0494957d4ade24fedd74619566cc777927e82c45
Nathália Pissuti <nathaliapissuti@gmail.com>
2023-05-27 11:47:50 -0300

    Loading pokemon

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import 'package:pokedex/models/pokemon_model.dart';
+import 'package:pokedex/services/pokemon_service.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -9,6 +12,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late List<PokemonModel?> _pokemonList = [];
+  bool _showLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
+  void _getData() async {
+    _pokemonList = (await PokemonService().getPokemonList());
+    Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {
+          _showLoading = false;
+        }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +39,9 @@ class _HomeScreenState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Pokemon list',
+              _showLoading
+                  ? 'Loading...'
+                  : 'Pokemon list ${_pokemonList.length}',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
