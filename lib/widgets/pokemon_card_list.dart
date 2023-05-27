58ad719e4f07b0481dbc9f4750b12fc9675d0fee
Nathália Pissuti <nathaliapissuti@gmail.com>
2023-05-27 19:34:17 -0300

    Added PokemonCardList

---
+++ lib/widgets/pokemon_card_list.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../models/pokemon_model.dart';
+import 'pokemon_card.dart';
+
+class PokemonCardList extends StatelessWidget {
+  final List<PokemonModel> pokemonList;
+
+  const PokemonCardList({
+    super.key,
+    required this.pokemonList,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        for (var pokemon in pokemonList)
+          PokemonCard(
+            pokemon: pokemon,
+            context: context,
+          )
+      ],
+    );
+  }
+}
