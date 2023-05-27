a090081e338f0a895088976cbc754b8b8cea41d8
Nathália Pissuti <nathaliapissuti@gmail.com>
2023-05-27 12:09:49 -0300

    Refactor in pokemon service

--- lib/services/pokemon_service.dart
+++ lib/services/pokemon_service.dart
@@ -10,7 +10,42 @@ class ApiConstants {
 }
 
 class PokemonService {
-  Future<PokemonModel?> getPokemon(int id) async {
+  Future<String?> _getEvolutionChainUrl(PokemonModel pokemon) async {
+    var url = Uri.parse(pokemon.species.url);
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      SpeciesModel species = speciesFromJson(response.body);
+
+      return species.evolutionChain.url;
+    }
+
+    return null;
+  }
+
+  Future<EvolutionChainModel?> _getEvolutionChain(
+    PokemonModel pokemon,
+  ) async {
+    var urlEvolutionChain = await _getEvolutionChainUrl(pokemon);
+
+    if (urlEvolutionChain == null) {
+      return null;
+    }
+
+    var url = Uri.parse(urlEvolutionChain);
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      EvolutionChainModel evolutionChain =
+          evolutionChainFromJson(response.body);
+
+      return evolutionChain;
+    }
+
+    return null;
+  }
+
+  Future<PokemonModel?> _getPokemon(int id) async {
     try {
       var url = Uri.parse(
           '${ApiConstants.baseUrl}${ApiConstants.pokemonEndpoint}/$id');
@@ -18,21 +53,10 @@ class PokemonService {
       if (response.statusCode == 200) {
         PokemonModel pokemon = pokemonFromJson(response.body);
 
-        var urlSpecies = Uri.parse(pokemon.species.url);
-        var responseSpecies = await http.get(urlSpecies);
+        EvolutionChainModel? evolutionChain = await _getEvolutionChain(pokemon);
 
-        if (responseSpecies.statusCode == 200) {
-          SpeciesModel species = speciesFromJson(responseSpecies.body);
-
-          var urlEvolutionChain = Uri.parse(species.evolutionChain.url);
-          var responseEvolutionChain = await http.get(urlEvolutionChain);
-
-          if (responseEvolutionChain.statusCode == 200) {
-            EvolutionChainModel evolutionChain =
-                evolutionChainFromJson(responseEvolutionChain.body);
-
-            pokemon.evolutionChain = evolutionChain;
-          }
+        if (evolutionChain != null) {
+          pokemon.evolutionChain = evolutionChain;
         }
 
         return pokemon;
@@ -46,6 +70,6 @@ class PokemonService {
 
   Future<List<PokemonModel?>> getPokemonList() async {
     var numbers = List<int>.generate(150, (i) => i + 1);
-    return await Future.wait(numbers.map((number) => getPokemon(number)));
+    return await Future.wait(numbers.map((number) => _getPokemon(number)));
   }
 }
