d0821a23775b91059705a42bf8847455259b138c
Nathália Pissuti <nathaliapissuti@gmail.com>
2023-05-28 19:34:52 -0300

    Changed title screen to receive margin properties

--- lib/widgets/page_title.dart
+++ lib/widgets/page_title.dart
@@ -3,17 +3,23 @@ import 'package:flutter/material.dart';
 class PageTitle extends StatelessWidget {
   final Color color;
   final String text;
+  final bool withToolbar;
+  final double marginBottom;
 
   const PageTitle({
     super.key,
     required this.color,
     required this.text,
+    required this.withToolbar,
+    required this.marginBottom,
   });
 
   @override
   Widget build(BuildContext context) {
+    double marginTop = withToolbar ? 8 : 64;
+
     return Container(
-      margin: const EdgeInsets.fromLTRB(16, 64, 16, 32),
+      margin: EdgeInsets.fromLTRB(16, marginTop, 16, marginBottom),
       child: Text(
         text,
         style: TextStyle(
